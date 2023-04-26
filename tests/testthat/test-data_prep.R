c241c51bc43b97c2f06220017295e41575d1fe73
zsmith27 <zsmith2727@gmail.com>
2023-04-26 12:16:31 -0400

    Added test data and unit tests.

---
+++ tests/testthat/test-data_prep.R
@@ -0,0 +1,41 @@
+test_that("multiplication works", {
+  library(readr)
+  usgs_df <- readr::read_csv(here::here("inst",
+                                        "usgs_4_26_23.csv"),
+                             col_types = readr::cols(
+                               ...1 = col_double(),
+                               MSSIH_BIOSAMPLE_COLLECT_METHOD = col_character(),
+                               MSSIH_EVENT_SMAS_HISTORY_ID = col_character(),
+                               MSSIH_REPLICATE = col_double(),
+                               MSSIH_EVENT_SMAS_SAMPLE_DATE = col_character(),
+                               MSDH_GENSPECIES = col_character(),
+                               MSDH_INDIVIDUAL_SPECIES_CNT = col_double(),
+                               MSSIH_TOTAL_INDIV_CNT_IND = col_double(),
+                               BASIN = col_character(),
+                               LOCATION = col_character(),
+                               RIVMILE = col_character(),
+                               COLL_DATE = col_character(),
+                               Replicate = col_double(),
+                               MACRO_GENSPECIES = col_character(),
+                               INDIV = col_double(),
+                               site_id = col_character(),
+                               ORIGINAL_BIOSAMPLE_COLLECT_METHOD_ID = col_double(),
+                               BIOSAMPLE_COLLECT_METHOD_ID = col_double(),
+                               BIOSAMPLE_COLLECT_METH_DESC = col_character(),
+                               COLLECT = col_double()
+                             )) |>
+    dplyr::select(-"...1")
+
+  prepped_df <- BAP::data_prep(usgs_df)
+
+  expect_equal(
+    sort(unique(prepped_df$MSSIH_EVENT_SMAS_HISTORY_ID)),
+    sort(unique(usgs_df$MSSIH_EVENT_SMAS_HISTORY_ID))
+  )
+
+  expect_equal(
+    nrow(prepped_df),
+    nrow(usgs_df)
+  )
+
+})
