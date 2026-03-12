7672dd758091ea9d4b23fc8c8219eb64080774d1
zsmith27 <zachary.smith2@dec.ny.gov>
2026-03-12 15:27:43 -0400

    #11 a solution towards identifying oligochaeta for the NCO calculation due to oligochatea no longer being represented in the master species list.

--- R/nco_rich.R
+++ R/nco_rich.R
@@ -10,12 +10,21 @@
 
 rich_nco <- function(Long, Level = "GENUS"){
   Taxa.df <- wide(Long, Level)
+
+  # Get Chiro taxa
   FAM <- split(Long[, Level], Long$FAMILY)
   taxa.list_c <- unique(FAM$CHIRONOMIDAE)
-  PHYLUM <- split(Long[, Level], Long$PHYLUM)
-  taxa.list_o <- unique(PHYLUM$ANNELIDA)
+
+  # Get Oligo Taxa
+  Order <- split(Long[, Level], Long$ORDER)
+  lumbriculida <- unique(Order$LUMBRICULIDA)
+  tubificida <- unique(Order$TUBIFICIDA)
+  enchytraeidae <- unique(FAM$ENCHYTRAEIDAE)
+  taxa.list_o <- list(lumbriculida, tubificida, enchytraeidae)
+
+  # Combine the lists
   taxa.list <- list(taxa.list_c, taxa.list_o)
-  group.rich <- group_rich(taxa.list, Taxa.df)
+  group.rich <- group_rich(NameList = taxa.list, taxa.df = Taxa.df)
   total.rich <- vegan::specnumber(Taxa.df[, 6:ncol(Taxa.df)])
   return(total.rich - group.rich)
 }
